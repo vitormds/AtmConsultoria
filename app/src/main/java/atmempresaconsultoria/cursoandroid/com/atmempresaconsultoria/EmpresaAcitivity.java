ae26b8da955ab03f2b9145bc30361832b171619c
Vitor Souza <vitormendes.souza@hotmail.com>
2018-07-17 16:47:00 -0300

    Primeiro commit

---
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/EmpresaAcitivity.java
@@ -0,0 +1,14 @@
+package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class EmpresaAcitivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa_acitivity);
+    }
+}
